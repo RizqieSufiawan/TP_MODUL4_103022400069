fb2d7436ecf0c819602199bb78fa313f613b54ec
RizqieSufiawan <iamrizqie@gmail.com>
2026-03-16 02:49:38 +0700

    second commit

---
+++ KodePos.cs
@@ -0,0 +1,32 @@
+﻿namespace TP_MODUL4_103022400069
+{
+    internal class KodePos
+    {
+        public string GetKodePos(string kelurahan)
+        {
+            string[,] tabelKodePos =
+            {
+                { "Batununggal", "40266" },
+                { "Kujangsari",  "40287" },
+                { "Mengger",     "40267" },
+                { "Wates",       "40256" },
+                { "Cijaura",     "40287" },
+                { "Jatisari",    "40286" },
+                { "Margasari",   "40286" },
+                { "Sekejati",    "40286" },
+                { "Kebonwaru",   "40272" },
+                { "Maleer",      "40274" }
+            };
+
+            for (int i = 0; i < tabelKodePos.GetLength(0); i++)
+            {
+                if (tabelKodePos[i, 0].Equals(kelurahan, StringComparison.OrdinalIgnoreCase))
+                {
+                    return tabelKodePos[i, 1];
+                }
+            }
+
+            return "Kelurahan tidak ditemukan";
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            KodePos kodePos = new KodePos();
+
+            Console.WriteLine("Batununggal : " + kodePos.GetKodePos("Batununggal"));
+            Console.WriteLine("Kujangsari  : " + kodePos.GetKodePos("Kujangsari"));
+            Console.WriteLine("Mengger     : " + kodePos.GetKodePos("Mengger"));
+            Console.WriteLine("Wates       : " + kodePos.GetKodePos("Wates"));
+            Console.WriteLine("Cijaura     : " + kodePos.GetKodePos("Cijaura"));
+            Console.WriteLine("Jatisari    : " + kodePos.GetKodePos("Jatisari"));
+            Console.WriteLine("Margasari   : " + kodePos.GetKodePos("Margasari"));
+            Console.WriteLine("Sekejati    : " + kodePos.GetKodePos("Sekejati"));
+            Console.WriteLine("Kebonwaru   : " + kodePos.GetKodePos("Kebonwaru"));
+            Console.WriteLine("Maleer      : " + kodePos.GetKodePos("Maleer"));
         }
     }
 }
